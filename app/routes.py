23d2cde8a188b770a7858cef9bbe77e0aed5b1fe
Hannah Eason <silentvalley.hannah@gmail.com>
2023-04-22 16:19:52 -0500

    sign in form

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,14 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Hannah'}
     return render_template('index.html', user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
