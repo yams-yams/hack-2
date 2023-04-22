f2c96ed38b2b5952efe2c4f7abb444946e17e658
Hannah Eason <silentvalley.hannah@gmail.com>
2023-04-22 15:11:55 -0500

    Templates started

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Hannah'}
+    return render_template('index.html', user=user)
