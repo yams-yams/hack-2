b4259e91f6e8dea6c662338ac023fc6a2b9b5e2b
Hannah Eason <silentvalley.hannah@gmail.com>
2023-04-22 16:32:28 -0500

    logins

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app.forms import LoginForm
 
 @app.route('/')
@@ -8,7 +8,10 @@ def index():
     user = {'username': 'Hannah'}
     return render_template('index.html', user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
