953b2b75291e170b60143934b74d62666d9cbaa2
Hannah Eason <silentvalley.hannah@gmail.com>
2023-04-22 18:12:14 -0500

    Register Form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, FileField
+from wtforms.validators import DataRequired, NumberRange, Optional
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    year = IntegerField('Catalog Year (What Academic Year Did You Start Your Degree)', validators=[DataRequired(),NumberRange(min=2013,max=2023)])
+    file = FileField('Degree Plan', validators=[Optional(strip_whitespace=True)])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Register')
+
+class RequestForm(FlaskForm):
+    query = StringField('What is your question?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
-from app.forms import LoginForm
+from app.forms import LoginForm, RegisterForm
 
 @app.route('/')
 @app.route('/index')
@@ -15,3 +15,11 @@ def login():
         flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data))
         return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        flash('Registration requested for user {}, remember_me={}'.format(form.username.data, form.year.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('register.html', title='Sign Up', form=form)
